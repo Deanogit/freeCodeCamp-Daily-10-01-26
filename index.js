95ee695b0129ff945dc517dff538fca245fe2a16
Dean Howe <hideanhowe@gmail.com>
2026-01-20 19:52:10 +0000

    testing patterns

--- index.js
+++ index.js
@@ -9,3 +9,40 @@
 // "X wins" if player X has three in a row.
 // "O wins" if player O has three in a row.
 // "Draw" if no player has three in a row.
+
+// function ticTacToe(board) {
+//   console.log(board);
+
+//   const pattern1 = [
+//     ['1', '1', '1'],
+//     ['0', '0', '0'],
+//     ['0', '0', '0'],
+//   ];
+
+//   // return board;
+// }
+
+function ticTacToe(board) {
+  console.log(board);
+
+  // const pattern1 = [[
+  //  "1", "1", "1"
+  // ], ["0", "0", "0"],
+  // ["0", "0", "0"]];
+
+  // Horizontal coordinates
+  // board[0][0], board[0][1], board[0][2],
+  // board[1][0], board[1][1], board[1][2],
+  // board[2][0], board[2][1], board[2][2],
+  ////
+  // Vertical coordinates
+  // board[0][0], board[1][0], board[2][0],
+  // board[0][1], board[1][1], board[2][1],
+  // board[0][2], board[1][2], board[2][2],
+  ////
+  // Diagonal coordinates
+  // board[0][0], board[1][1], board[2][2],
+  // board[2][0], board[1][1], board[0][2],
+
+  // return board;
+}
